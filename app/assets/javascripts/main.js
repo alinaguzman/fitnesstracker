ded79eff9df21823ebd5b9b2a3461d8a5ffea636
Alina Guzman <alinochka8@gmail.com>
2016-03-24 09:57:19 -0400

    fix to find coordinates based on input distance

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,12 +6,10 @@ var gps = {
     var r = meters/111300
       , y0 = original_lat
       , x0 = original_lng
-      , u = Math.random()
       , v = Math.random()
-      , w = r * Math.sqrt(u)
       , t = 2 * Math.PI * v
-      , x = w * Math.cos(t)
-      , y1 = w * Math.sin(t)
+      , x = r * Math.cos(t)
+      , y1 = r * Math.sin(t)
       , x1 = x / Math.cos(y0)
 
     newY = y0 + y1;
